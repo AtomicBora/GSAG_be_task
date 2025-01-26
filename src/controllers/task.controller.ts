259edbe7fa8e593ac252cb85809caaf7371d7ea1
Borivoje <devborivoje@gmail.com>
2025-01-26 16:02:47 +0100

    Separate busines logic from task controller

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,7 +1,6 @@
 import { CustomRequest, DecodedToken } from '#@/middleware/auth';
+import { createUserTaskService } from '#@/services/task.service';
 import { Task } from '#@/types/Task';
-import { UserTask } from '#@/types/User';
-import poolClient from '#@/utils/createDBPool';
 import logger from '#@/utils/logger';
 import { findByEmail } from '#@/utils/query.helpers';
 import { Request, Response } from 'express';
@@ -26,35 +25,16 @@ const createUserTask = async (
 
 		const userId = user.id;
 
-        // TODO: move logic to task service
+		const task: Partial<Task> = {
+			description,
+			priority,
+			status,
+			title
+		};
 
-		// TODO: create helper function to insert task
-		const result = await poolClient.query<Task>(
-			'INSERT INTO task_GS (title, description, priority, status) VALUES ($1, $2, $3, $4) RETURNING *',
-			[title, description, priority, status]
-		);
+		const createdTask = await createUserTaskService(userId, task);
 
-		const taskId = result.rows[0].id;
-
-		//TODO: create helper function to link task with user
-		const jointTableUpdateResult = await poolClient.query<
-			Pick<UserTask, 'task_id' | 'user_id'>
-		>('INSERT INTO user_task_GS (user_id, task_id) VALUES ($1, $2)', [
-			userId,
-			taskId
-		]);
-
-		logger.info({ jointTableUpdateResult });
-
-		if (jointTableUpdateResult.rowCount === 0) {
-			logger.error(`Error linking task with user with email ${email}!`);
-		}
-
-		logger.info(
-			`Task with id ${taskId.toString()} created for user with email ${email}!`
-		);
-
-		res.status(201).json(result.rows[0]);
+		res.status(201).json(createdTask);
 	} catch (error) {
 		logger.error(`Error creating task for user with email ${email}!`);
 		logger.error(error);
---
+++ src/services/task.service.ts
@@ -0,0 +1,37 @@
+import { Task } from '#@/types/Task';
+import { UserTask } from '#@/types/User';
+import poolClient from '#@/utils/createDBPool';
+import logger from '#@/utils/logger';
+
+const createUserTaskService = async (userId: number, task: Partial<Task>) => {
+	const { description, priority, status, title } = task;
+
+	// TODO: create helper function to insert task
+	const result = await poolClient.query<Task>(
+		'INSERT INTO task_GS (title, description, priority, status) VALUES ($1, $2, $3, $4) RETURNING *',
+		[title, description, priority, status]
+	);
+
+	const taskId = result.rows[0].id;
+
+	const jointTableUpdateResult = await poolClient.query<
+		Pick<UserTask, 'task_id' | 'user_id'>
+	>('INSERT INTO user_task_GS (user_id, task_id) VALUES ($1, $2)', [
+		userId,
+		taskId
+	]);
+
+	if (jointTableUpdateResult.rowCount === 0) {
+		logger.error(`Error linking task for user ${userId.toString()}!`);
+	}
+
+	// ovo bi moglo da se stavi u neki log fajl da se prati ko je kada dodao sta i eventualno napravi neki revision history
+    // ovako je nepotrebno i nepozeljno
+	logger.info(
+		`Task with id ${taskId.toString()} created for user ${userId.toString()}!`
+	);
+
+	return result.rows[0];
+};
+
+export { createUserTaskService };
